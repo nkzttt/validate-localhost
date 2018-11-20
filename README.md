c9442d7322d18a1c5bd2a446fbb673ad33dddaca
nakazato hiroki <b10214@pc-10832.cyberagent.co.jp>
2018-11-20 17:42:34 +0900

    [update] added options.

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Validate Page
 
 Getting html from page of localhost then validate the code.  
+
+## Options
+
+It's able to pass options via command line arguments.
+
+- `--port={port}` - set the `port`. default is `3000`.
+- `--secure` - if passed `--secure` argument then set the protocol to `https`. 
+- `--pc` - if passed `--pc` argument UserAgent is behaved like it. this is default.
+- `--sp` - if passed `--sp` argument UserAgent is behaved like it.
+- `--tb` - if passed `--tb` argument UserAgent is behaved like it.
--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const rp = require('request-promise');
 const validator = require('html-validator');
+const argv = require('minimist')(process.argv.slice(2));
+
+const UA = {
+  PC: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36',
+  SP: 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1',
+  TB: 'Mozilla/5.0 (iPad; CPU OS 11_0 like Mac OS X) AppleWebKit/604.1.34 (KHTML, like Gecko) Version/11.0 Mobile/15A5341f Safari/604.1'
+};
 
 (async () => {
+  // set variables
+  const protocol = argv.secure ? 'https' : 'http';
+  const port = argv.port || '3000';
+  const ua = argv.pc ? UA.PC : argv.sp ? UA.SP : argv.tb ? UA.TB : UA.PC;
+
   // trying to get html
   const requestOptions = {
-    uri: 'http://localhost:3000',
+    uri: `${protocol}://localhost:${port}`,
     headers: {
-      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36'
+      'User-Agent': ua
     }
   };
   let html;
