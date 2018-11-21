5a5d32f1634b600521a13885156259a0dbdb876c
nakazato hiroki <b10214@pc-10832.cyberagent.co.jp>
2018-11-21 19:16:49 +0900

    [update] updated to display log.

--- bin/validate.js
+++ bin/validate.js
@@ -1,7 +1,16 @@
 #! /usr/bin/env node
 
+const log = require('npmlog');
 const argv = require('minimist')(process.argv.slice(2));
 const { secure, port, path, pc, sp, tb } = argv;
 const validate = require('../');
 
-validate({ secure, port, path, pc, sp, tb }).then(console.log).catch(console.error);
+validate({ secure, port, path, pc, sp, tb }).then(result => {
+  if (result.messages.length) {
+    result.messages.forEach(error => {
+      log.error(`${error.type} at line ${error.lastLine}: ${error.message}`);
+    });
+  } else {
+    console.log('no error');
+  }
+}).catch(log.error);
